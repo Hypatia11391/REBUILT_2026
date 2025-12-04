5e31cd61176e529749acf7acf37decc2780b73a6
Pavel <pavel.timofeev2008@gmail.com>
2025-12-03 17:50:32 -0800

    made a code for driving (in theory)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,9 +14,12 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain.DriveBase;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.DriveWithJoystick;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -32,9 +35,11 @@ public class RobotContainer {
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
+  private final DriveBase m_driveBase = new DriveBase();
+  private final Joystick m_stick = new Joystick(0);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
+    m_driveBase.setDefaultCommand(new DriveWithJoystick(m_driveBase, m_stick));
     configureBindings();
   }
 
--- src/main/java/frc/robot/commands/DriveWithJoystick.java
+++ src/main/java/frc/robot/commands/DriveWithJoystick.java
@@ -11,7 +11,37 @@
  **/
 
 package frc.robot.commands;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveTrain.DriveBase;
 
-public class DriveWithJoystick {
-    
+
+public class DriveWithJoystick extends Command{
+
+    private final DriveBase m_drive;
+    private final Joystick m_stick; // joystick object
+
+    public DriveWithJoystick(DriveBase drive, Joystick stick){
+        m_drive = drive;
+        m_stick = stick;
+        addRequirements(m_drive);
+  }
+  @Override
+  public void initialize(){}
+
+  @Override
+  public void execute(){
+    double ySpeed = -m_stick.getY();
+    double xSpeed = -m_stick.getX();
+    double zRot = -m_stick.getZ();
+    m_drive.drive(xSpeed, ySpeed, zRot);
+  }
+  @Override
+  public void end(boolean interrupted){
+    m_drive.drive(0,0,0);
+  }
+  @Override
+  public boolean isFinished(){
+    return false;
+  }
 }
