13068efb27c4964b971a93cd7597aaf85190e081
Sasha Mattison <sashastoolbox@gmail.com>
2026-05-11 09:52:53 -0700

    Pathplanner initial setup

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,6 +1,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos extends Command{
 
@@ -22,6 +25,20 @@ public final class Autos extends Command{
   @Override
   public void end(boolean interrupted) {
 
+  }
+
+  public Command loadPath() {
+
+    try {
+
+      PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path");
+
+      return AutoBuilder.followPath(path);
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        return Commands.none();
+    }
   }
 
 
